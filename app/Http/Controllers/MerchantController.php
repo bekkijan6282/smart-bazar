819d9b64c7cd0286483f858d543468b783aadec5
bekkijan6282 <b.kuziyev22@gmail.com>
2024-11-01 01:22:58 +0500

    Request validators created

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -2,47 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Merchant\MerchantRequest;
+use App\Http\Requests\Merchant\UpdateMerchantRequest;
+use App\Models\Role;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MerchantController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('access.merchant', ['only' => ['show', 'destroy']]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function index(Request $request): JsonResponse
     {
-        //
+        $role = Role::where('name', 'merchant')->first();
+        $merchants = User::where('role_id', $role->id)->where('id', auth()->id())->filter($request)->with('role')->get();
+
+        return response()->json([
+            'data' => $merchants,
+        ], JsonResponse::HTTP_OK);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function store(MerchantRequest $request): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        $role = Role::where('name', 'merchant')->first();
+        $validated['role_id'] = $role->id;
+        $validated['password'] = bcrypt('123456789');
+        $validated['registered_at'] = Carbon::now();
+        $user = User::create($validated);
+
+        return response()->json([
+            'data' => $user,
+        ], JsonResponse::HTTP_CREATED);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function show(User $merchant): JsonResponse
     {
-        //
+        return response()->json([
+            'data' => $merchant->load('role'),
+        ], JsonResponse::HTTP_OK);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function update(UpdateMerchantRequest $request, User $merchant): JsonResponse
     {
-        //
+        $validated = $request->validated();
+
+        if (isset($validated['password'])) {
+            $validated['password'] = bcrypt($validated['password']);
+        }
+
+        $merchant->update($validated);
+
+        return response()->json([
+            'data' => $merchant,
+        ], JsonResponse::HTTP_OK);
+    }
+
+    public function destroy(User $merchant): JsonResponse
+    {
+        $merchant->delete();
+
+        return response()->json([], JsonResponse::HTTP_NO_CONTENT);
     }
 }
---
+++ app/Http/Requests/Shop/UpdateShoptRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Shop;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Shop;
+
+class UpdateShoptRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $shopId = $this->route('shop')->id;
+        $shop = Shop::find($shopId);
+        return $shop->merchant_id === auth()->id();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'address' => 'sometimes|string',
+            'schedule' => 'sometimes|string',
+            'latitude' => 'sometimes',
+            'longitude' => 'sometimes',
+        ];
+    }
+}
